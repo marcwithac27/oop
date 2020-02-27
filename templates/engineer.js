242787007b4e8a9cb628bccf5c05c7774586ddbc
Marc Maldonado <marc@telecomelite.com>
2020-02-27 08:58:54 -0500

    OK For real Alomost done

--- templates/engineer.js
+++ templates/engineer.js
@@ -2,7 +2,7 @@ exports.generateHTML = function(data) {
     return `<div class="card float-left bg-success" style="width: 18rem;">    
     <div class="card">
       <h2 class="card-header bg-secondary">${data.name}</h5>
-      <h5 class="card-text"><i class="fab fa-wpbeginner">${data.getRole()}</i> </h5>
+      <h5 class="card-text bg-success"><i class="fab fa-wpbeginner">${data.getRole()}</i> </h5>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item text-info">ID: ${data.id} </li>
--- templates/intern.js
+++ templates/intern.js
@@ -2,7 +2,7 @@ exports.generateHTML = function(data) {
     return `<div class="card float-left bg-success" style="width: 18rem;">    
     <div class="card">
       <h2 class="card-header bg-primary">${data.name}</h5>
-      <h5 class="card-text"><i class="fal fa-transgender-alt">${data.getRole()}</i></h5>
+      <h5 class="card-text bg-success"><i class="fal fa-transgender-alt">${data.getRole()}</i></h5>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item text-info">ID: ${data.id} </li>
