0d0e9ac8c26e59aeff3a29a8cab48401b21c384c
Marc Maldonado <marc@telecomelite.com>
2020-02-26 19:27:41 -0500

    Test finally passed

--- templates/manager.js
+++ templates/manager.js
@@ -1,8 +1,8 @@
 exports.generateHTML = function(data) {
-    return `<div class="card float-left" style="width: 18rem;">    
+    return `<div class="card float-left bg-success" style="width: 18rem;">    
     <div class="card-body">
       <h2 class="card-title">${data.name}</h5>
-      <h5 class="card-text"><i class="fas fa-mug-hot"></i> ${data.getRole()}</h5>
+      <h5 class="card-text"><i class="fas fa-mug-marshmallows">${data.getRole()}</i></h5>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">ID: ${data.id} </li>
